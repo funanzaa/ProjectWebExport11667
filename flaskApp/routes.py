108afb937b375de9c76eda97ceab9e2ecc167ae7
funanzaa <eakachai_golf@hotmail.com>
2022-01-22 18:44:55 +0700

    login pages

---
+++ flaskApp/routes.py
@@ -0,0 +1,25 @@
+from flask import render_template, url_for, flash, redirect
+from flaskApp import app
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('login.html', posts=posts)
